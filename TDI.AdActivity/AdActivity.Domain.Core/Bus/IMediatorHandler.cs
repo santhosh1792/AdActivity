3752a9aea6c310b5a337a7188a06d8681c7248d1
Santhosh <sdwarakanath@outlook.com>
2020-07-17 03:04:28 -0700

    Adding Domain.Core and Implementation of InMemory Bus

---
+++ TDI.AdActivity/AdActivity.Domain.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,13 @@
+﻿using AdActivity.Domain.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdActivity.Domain.Core.Bus
+{
+    public interface IMediatorHandler
+    {
+        Task SendCommand<T>(T command) where T : Command;
+    }
+}
